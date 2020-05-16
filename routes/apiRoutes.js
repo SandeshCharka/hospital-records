707c80fbb5b292a41a735979cce41a8f10dd7ef4
sandesh <sd.charka@gmail.com>
2020-05-16 00:49:28 -0700

    formatting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,51 +1,50 @@
 var medicaldb = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
 
   //Get all patients
-  app.get("/api/patients", function (req, res) {
-    medicaldb.Patient.findAll().then(function (results) {
+  app.get("/api/patients", function(req, res) {
+    medicaldb.Patient.findAll().then(function(results) {
       res.json(results);
     });
   });
 
   // Find Patient by ID
-  app.get("/api/patients/:id", function (req, res) {
+  app.get("/api/patients/:id", function(req, res) {
     var id = req.params.id
 
     medicaldb.Patient.findAll({
       where: {
         id: id,
       }
-    }).then(function (results) {
+    }).then(function(results) {
       res.json(results);
     });
   });
 
   // Find Doctor by ID 
-  app.get("/api/doctors/:id", function (req, res) {
+  app.get("/api/doctors/:id", function(req, res) {
     var id = req.params.id
 
     medicaldb.Doctor.findAll({
       where: {
         id: id,
       }
-    }).then(function (results) {
+    }).then(function(results) {
       res.json(results);
     });
   });
 
   // Find Patient by DOC ID
-  app.get("/api/patients/doc/:id", function (req, res) {
+  app.get("/api/patients/doc/:id", function(req, res) {
     var id = req.params.id
 
     medicaldb.Patient.findAll({
       where: {
         DoctorId: id,
       }
-    }).then(function (results) {
+    }).then(function(results) {
       res.json(results);
     });
   });
-
 };
